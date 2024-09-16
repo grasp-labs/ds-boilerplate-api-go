8984e0631170cd9df34b6bc164d1448ef8b3619e
yuan1979 <yuan1979@gmail.com>
2024-09-16 13:10:32 +0200

    update config

--- config/config.go
+++ config/config.go
@@ -2,7 +2,6 @@ package config
 
 import (
 	"github.com/google/uuid"
-	"net/url"
 )
 
 // Config defines the config for ds servers
@@ -13,10 +12,6 @@ type Config struct {
 	Port string `default:":8080"` // default to 8080
 	// Host defines the host for the server
 	Host string `default:"localhost"` // default to localhost
-	// Auth defines the auth for the server
-	AuthServer *url.URL
-	// Entitlement Server defines the entitlement server
-	EntitlementServer *url.URL
 	// Version defines the version for the service
 	Version string `default:"v1"` // default to v1
 	// App path
---
+++ main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"github.com/creasty/defaults"
+	"github.com/grasp-labs/dsserver/config"
+	"github.com/grasp-labs/dsserver/models"
+	"github.com/grasp-labs/dsserver/server"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/gommon/log"
+	"net/http"
+)
+
+type SampleController struct {
+	*models.DefaultController
+}
+
+func NewSampleController() models.Controller {
+	return &SampleController{}
+}
+
+func (c *SampleController) GetRoutes() models.Routes {
+	routes := models.Routes{
+		models.Route{
+			Path:                      "/test",
+			Method:                    "GET",
+			AllowUnauthenticatedUsers: false,
+			HandlerFunc: func(c echo.Context) error {
+				return c.JSON(http.StatusOK, "ok")
+			},
+			RequiredPermissions: []string{"service.log.admin"},
+		},
+	}
+	return routes
+}
+
+func main() {
+	cfg := config.Config{
+		AppRootPath: "/app",
+		Port:        ":8081",
+	}
+
+	err := defaults.Set(&cfg)
+	log.Infof("Config: %+v", cfg)
+	if err != nil {
+		return
+	}
+	srv := server.NewServer(&cfg)
+
+	sampleController := NewSampleController()
+	server.RegisterRoutes(&cfg, server.Protected, sampleController)
+
+	for _, route := range srv.Routes() {
+		fmt.Printf("Method: %s, Path: %s\n", route.Method, route.Path)
+	}
+
+	if err := srv.Start(":8081"); !errors.Is(err, http.ErrServerClosed) {
+		log.Fatal(err)
+	}
+
+	log.Infof("Server started on port %s", cfg.Port)
+}
